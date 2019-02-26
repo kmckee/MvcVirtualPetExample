2fbcf4cc38d51054c6f156d1a097f5c1d9ed2640
Kyle McKee <mckee.kyle@gmail.com>
2019-02-26 10:34:49 -0500

    First commit

---
+++ MvcVirtualPets.Tests/PetControllerTests.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using MvcVirtualPets.Controllers;
+using MvcVirtualPets.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace MvcVirtualPets.Tests
+{
+    public class PetControllerTests
+    {
+        PetController underTest;
+        public PetControllerTests()
+        {
+            underTest = new PetController();
+        }
+
+        [Fact]
+        public void Index_Returns_A_ViewResult()
+        {
+            var result = underTest.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Index_Model_Has_3_Pets()
+        {
+            var result = underTest.Index();
+            var model = (IEnumerable<Pet>)result.Model;
+            Assert.Equal(3, model.Count());
+        }
+
+        [Fact]
+        public void Details_Model_Has_Correct_Id()
+        {
+            var expectedId = 2;
+            var result = underTest.Details(expectedId);
+            var model = (Pet)result.Model;
+            Assert.Equal(expectedId, model.Id);
+        }
+    }
+}
---
+++ MvcVirtualPets/Controllers/PetController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcVirtualPets.Models;
+using MvcVirtualPets.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcVirtualPets.Controllers
+{
+    public class PetController : Controller
+    {
+        PetRepository petRepo;
+        public PetController()
+        {
+            petRepo = new PetRepository();
+        }
+
+        public ViewResult Index()
+        {
+            var model = petRepo.GetAll(); 
+            return View(model);
+        }
+
+        public ViewResult Details(int id)
+        {
+            var model = petRepo.GetById(id); 
+            return View(model);
+        }
+    }
+}
